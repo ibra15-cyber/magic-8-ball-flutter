a8cb0f45ebf18abaf56009b777c01149f5e4dcb1
ibra15-cyber <91777628+ibra15-cyber@users.noreply.github.com>
2021-12-03 09:44:52 +0000

    8 ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,51 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
+int ballNumber = 1;
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+            backgroundColor: Colors.lightBlue,
+          appBar: AppBar(
+            title: Center(
+              child: Text(
+                "Ask me any question",
+                // textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          body: MyBody(),
+        ),
       ),
     );
+class MyBody extends StatefulWidget {
+  // const MyBody({Key? key}) : super(key: key);
+
+  @override
+  _MyBodyState createState() => _MyBodyState();
+}
+
+class _MyBodyState extends State<MyBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: <Widget>[
+          Center(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(5) + 1;
+                });
+
+              },
+              child: Image.asset("images/ball$ballNumber.png",
+                width: 300,
+                height: 500,
+              ),
+            ),
+          ),
+        ]
+    );
+  }
+}
